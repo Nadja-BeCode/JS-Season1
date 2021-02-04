634ae6518a0ab823a130aa088ddcc492a2ff7b20
Nadja-BeCode <nadja.agostinho@gmail.com>
2021-02-04 16:38:07 +0100

    added

--- 04-dates/05-get-spooky-fridays/script.js
+++ 04-dates/05-get-spooky-fridays/script.js
@@ -5,5 +5,23 @@
 (() => {
 
     // your code here
+    document.getElementById("run").addEventListener("click",() => {
+    
+    let year = document.getElementById("year").value;//value pour recuperer ma valeur !!!
+    let arr =[];
 
+    //chercher sur l'annee le 13 de chaque mois
+        for (mois = 1 ; mois <= 12; mois++) {
+            let dateA = new Date(year, mois , 13);//i = paramètre de ma boucle
+    //verifier si le 13 est un vendredi
+            if (dateA.getDay() == 5) {
+              arr.push(mois);
+            }
+        }
+
+    //si c'est un vendredi les stockers dans une arr
+    //afficher là ou c'est stocké
+    //j'affiche comment ?(alert, log,...) 
+    alert(arr);
+    });
 })();
